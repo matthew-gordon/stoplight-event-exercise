a5f8e1eb77f17d21b967a02cb320c6491169f455
Matthew Gordon <laxredemption5525@gmail.com>
2016-10-15 15:35:34 -0600

    Completed exercises, did not finish stretch

--- script.js
+++ script.js
@@ -8,3 +8,40 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+var stop = document.getElementById('stopButton');
+var slow = document.getElementById('slowButton');
+var go = document.getElementById('goButton');
+var stopBulb = document.querySelector('div#stopLight.bulb');
+var slowBulb = document.querySelector('div#slowLight.bulb');
+var goBulb = document.querySelector('div#goLight.bulb');
+
+  function turnOnOff(e) {
+    if ( e.target !== e.currentTarget ) {
+      var clickedItem = e.target;
+      if ( e.target === stop) {
+          e.target.classList.add('on');
+          slow.classList.remove('on');
+          go.classList.remove('on');
+          stopBulb.style.backgroundColor= 'red';
+          slowBulb.style.backgroundColor= '#111';
+          goBulb.style.backgroundColor= '#111';
+      } else if ( e.target === slow ) {
+          e.target.classList.add('on');
+          stop.classList.remove('on');
+          go.classList.remove('on');
+          stopBulb.style.backgroundColor= '#111';
+          slowBulb.style.backgroundColor= 'yellow';
+          goBulb.style.backgroundColor= '#111';
+      } else if ( e.target === go ) {
+          e.target.classList.add('on');
+          stop.classList.remove('on');
+          slow.classList.remove('on');
+          stopBulb.style.backgroundColor= '#111';
+          slowBulb.style.backgroundColor= '#111';
+          goBulb.style.backgroundColor= 'green';
+      }
+    }
+    e.stopPropagation();
+  }
+document.getElementById('controls').addEventListener('click', turnOnOff, true);
